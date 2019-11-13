939fcfcd4a2c975421d48a32ee556af899e0cce2
Boris Nekezov <boris.nekezov@gmail.com>
2019-11-13 15:09:47 +0200

    Added fav icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+// import main javascript file
 import App from './js/app';
+// import main scss file
 import './scss/app.scss';
-
+// handle with coping the favicons in dist
+import '../public/assets/favicons/favicons';
 App();
